1bc2a2b2e643177ef7cdefe8a163ed8b0cdd7289
yuyang <summerkiss@me.com>
2017-07-01 01:28:07 -0500

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var usersob = require('./routes/usersob');
+var usersaa = require('./routes/usersaa');
+var error = require('./routes/error');
 
 var app = express();
 
@@ -25,6 +28,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/usersaa', usersaa);
+app.use('/usersob', usersob);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
